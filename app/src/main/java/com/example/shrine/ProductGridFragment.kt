662f6794f879a8f84dcbabb61055cc0f0dc15e44
dberl <dberlinches2003@gmail.com>
2025-10-24 16:40:17 +0200

    Completada cuarta parte del proyecto primeros pasos Material Design

--- app/src/main/java/com/example/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/example/shrine/ProductGridFragment.kt
@@ -1,15 +1,19 @@
 package com.example.shrine
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shrine.network.ProductEntry
@@ -31,8 +35,16 @@ class ProductGridFragment : Fragment() {
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
         val appBar = view.findViewById<Toolbar>(R.id.app_bar)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        val productGrid = view.findViewById<NestedScrollView>(R.id.product_grid)
 
         (activity as AppCompatActivity).setSupportActionBar(appBar)
+        appBar.setNavigationOnClickListener(NavigationIconClickListener(
+            activity!!,
+            productGrid,
+            AccelerateDecelerateInterpolator(),
+            ContextCompat.getDrawable(context!!, R.drawable.shr_branded_menu),
+            ContextCompat.getDrawable(context!!, R.drawable.shr_close_menu)
+        ))
 
         recyclerView.setHasFixedSize(true)
 
@@ -50,6 +62,10 @@ class ProductGridFragment : Fragment() {
         val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
         recyclerView.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            productGrid.background = context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
         return view
     }
 
