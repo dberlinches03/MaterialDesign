6342cceb6859551e6373be76fdd8743428dec48d
dberl <dberlinches2003@gmail.com>
2025-10-24 13:09:06 +0200

    Subida parte 1 primeros pasos con material design

---
+++ app/src/main/java/com/example/shrine/LoginFragment.kt
@@ -0,0 +1,57 @@
+package com.example.shrine
+
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
+
+/**
+ * Fragment representing the login screen for Shrine.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        val passwordEditText = view.findViewById<EditText>(R.id.password_edit_text)
+        val passwordTextInput = view.findViewById<TextInputLayout>(R.id.password_text_input)
+        val nextButton = view.findViewById<Button>(R.id.next_button)
+
+
+
+        // Snippet from "Navigate to the next Fragment" section goes here.
+
+        nextButton.setOnClickListener {
+            if (!isPasswordValid(passwordEditText.text)) {
+                passwordTextInput.error = getString(R.string.shr_error_password)
+            } else {
+                passwordTextInput.error = null
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
+
+
+        passwordEditText.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(passwordEditText.text)) {
+                passwordTextInput.error = null
+            }
+            false
+        }
+
+        return view
+    }
+
+    // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
+
+}
